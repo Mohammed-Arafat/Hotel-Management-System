e0a5af30e936d2dd88e6294e898a4e7c1225d1af
Mohammed Arafat <mohammedarafat13579@gmail.com>
2023-12-17 01:20:54 +0600

    First

---
+++ src/Hotel/Management/System/connect.java
@@ -0,0 +1,18 @@
+package Hotel.Management.System;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class connect {
+    Connection connection;
+    Statement statement;
+
+    public connect(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelms", "root", "");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
